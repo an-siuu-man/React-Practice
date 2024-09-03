c7a5a8590f7a1d04a1c282380212eb3a3c4e0e1b
an-siuu-man <ansuman_sharma@protonmail.com>
2024-09-03 12:46:38 -0500

    added fade transition to page switching

--- junior-year/src/App.js
+++ junior-year/src/App.js
@@ -3,22 +3,41 @@ import Home from './components/Home';
 import Navbar from './components/navBar';
 import OurTeam from './components/OurTeam';
 import ContactUs from './components/ContactUs';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes, useLocation} from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+
+function AppRender() {
 
 
+  const location = useLocation();
 
-function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <div className="App-content"> 
-          <Routes>           
+        <TransitionGroup>
+          <CSSTransition
+            timeout={400}
+            classNames="page"
+            key={location.pathname}
+            >
+        <div className="page"> 
+          <Routes location={location}>
             <Route exact path="/" element= {<Home />} />
             <Route path="/ourteam" element= {<OurTeam />} />
             <Route path="/contactus" element= {<ContactUs />} />
           </Routes>
-          </div>        
+          </div>  
+          </CSSTransition>
+          </TransitionGroup>
+
+  );
+}
+
+
+const App =  () => {
+  return (
+    <Router>
+      <div className="App">
+          <Navbar />
+          <AppRender />
       </div>
     </Router>
   );
